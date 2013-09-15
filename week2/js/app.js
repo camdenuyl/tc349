43484476402751a97380ec0ad9e36530f227c72c
camdenuyl <camdenuyl@Cam-Den-Uyls-MacBook-Pro.local>
2013-09-15 17:17:58 -0400

    assignment 2

---
+++ week2/js/app.js
@@ -0,0 +1,33 @@
+// function hello() {
+// 	alert ("Hello, World!")
+// };
+
+
+var sBrowser, sUsrAg = navigator.userAgent;
+
+if(sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+} else if (sUsrAg.indexOf("Opera") > -1) {
+    sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Firefox") > -1) {
+    sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("MSIE") > -1) {
+    sBrowser = "Microsoft Internet Explorer";
+}
+
+
+
+function getBrowserId () {
+	alert("You are using: " + sBrowser);
+    var
+        aKeys = ["MSIE", "Firefox", "Safari", "Chrome", "Opera"],
+        sUsrAg = navigator.userAgent, nIdx = aKeys.length - 1;
+
+    for (nIdx; nIdx > -1 && sUsrAg.indexOf(aKeys[nIdx]) === -1; nIdx--);
+
+    return nIdx
+
+}
+
